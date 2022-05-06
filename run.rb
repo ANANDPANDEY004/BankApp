d5ac9c0d235e6c6c59263ee487fb45e0c1ba0854
ANANDPANDEY004 <Annupandey0808@gmail.com>
2022-05-06 16:20:00 +0530

    i

---
+++ run.rb
@@ -0,0 +1,11 @@
+require 'json'
+require 'Date'
+require_relative "./main.rb"
+require_relative "./user.rb"
+require_relative "./input.rb"
+require_relative "./account.rb"
+require_relative "./atm.rb"
+require_relative "./transaction.rb"
+# require_relative "./try.rb"
+ 
+Main.new.menu
